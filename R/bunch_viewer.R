e2e3dc9c252045fba3d3846a25fbf5b9e2a71141
Itai Trilnick <itai.trilnick@berkeley.edu>
2016-06-12 10:52:26 -0700

    added saving option to example

--- R/bunch_viewer.R
+++ R/bunch_viewer.R
@@ -25,7 +25,7 @@
 #' @examples
 #' ability_vec <- 4000 * rbeta(100000, 2, 5)
 #' earning_vec <- sapply(ability_vec, earning_fun, 0.2, 0.1, 0.2, 0, 1000)
-#' bunch_viewer(earning_vec, 1000, 20, 40, 2, 2, 20, TRUE)
+#' bunch_viewer(earning_vec, 1000, 20, 40, 2, 2, 20, TRUE, FALSE)
 #'
 #' @export
 #'
