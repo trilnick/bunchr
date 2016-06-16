ebe4e6ca2339babe6740de4e015a716cbdd2bd39
Itai Trilnick <itai.trilnick@berkeley.edu>
2016-06-16 10:11:33 -0700

    shortening example run time even more...

--- R/bunch.R
+++ R/bunch.R
@@ -45,17 +45,17 @@
 #' # analyzing a kink
 #' ability_vec <- 4000 * rbeta(100000, 2, 5)
 #' earning_vec <- sapply(ability_vec, earning_fun, 0.2, 0, 0.2, 0, 1000)
-#' # bunch_viewer(earning_vec, 1000, 40, 40, 1, 1, binw = 10)
-#' estim <- bunch(earning_vec, 1000, 0, 0.2, Tax = 0, 40, 40, 1, 1,
-#' binw = 10, draw=TRUE, nboots = 0, seed = 16)
+#' # bunch_viewer(earning_vec, 1000, 20, 20, 1, 1, binw = 20)
+#' estim <- bunch(earning_vec, 1000, 0, 0.2, Tax = 0, 20, 20, 1, 1,
+#' binw = 20, draw=TRUE, nboots = 0, seed = 16)
 #' estim$e
 #'
 #' # analyzing a notch
 #' earning_vec <- sapply(ability_vec, earning_fun, 0.2, 0.2, 0.2, 500, 1000)
-#' # bunch_viewer(earning_vec, 1000, 60, 150, 2, 100, binw = 10)
-#' estim <- bunch(earning_vec, 1000, 0.2, 0.2, Tax = 500, 60, 150, 2, 100,
-#' binw = 10, draw=TRUE, nboots = 0, seed = 16)
-#' estim$Bn
+#' bunch_viewer(earning_vec, 1000, 10, 40, 2, 22, binw = 50)
+#' estim <- bunch(earning_vec, 1000, 0.2, 0.2, Tax = 500, 10, 40, 2, 22,
+#' binw = 50, draw = FALSE, nboots = 0, seed = 16)
+#' estim$e
 #'
 #' @export
 
